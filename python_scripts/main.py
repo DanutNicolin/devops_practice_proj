2ee47b1072cbf72aacac0f553b7b72d7ef54687c
Nicolin Danut <nicolindanut@gmail.com>
2023-07-01 19:47:47 +0300

    formated structure

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -10,11 +10,12 @@ if __name__ == "__main__":
     table_data = TableScraper(target_page).get_table_data()
 
     db_manager = DBManager()
+    
     db_entryes = db_manager.fetch_data("exchange_rates")
     print(f"DB entryes: {len(db_entryes)}")
 
     db_manager.write_data(table_data, "exchange_rates")
-
     db_entryes = db_manager.fetch_data("exchange_rates")
     print(f"DB entryes after update: {len(db_entryes)}")
+
     db_manager.close_connection()
